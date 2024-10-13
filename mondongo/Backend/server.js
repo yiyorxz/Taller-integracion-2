db9b0249257c53f04344a32b504e755146ca37a9
javierrrp <javier.poblete2023@alu.uct.cl>
2024-10-13 01:37:20 -0300

    probando que muestre productos

--- mondongo/Backend/server.js
+++ mondongo/Backend/server.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 9000;
 const cors = require('cors');
 require('dotenv').config();
-
+import pool from './db';
 app.use(cors());
 app.use(express());
 
@@ -12,6 +12,15 @@ app.get("/", (req, res) => {
   res.send("Ola");
 });
 
+app.get("/api/products", async(req, res) => {
+  try{
+    const result = await pool.query('SELECT * FROM producto');
+    res.json(result.rows);
+  } catch (err){
+    console.error("error", err);
+    res.status(see).send("error")
+  }
+});
 
 
 app.listen(PORT, () => {
--- mondongo/src/pages/Homepage.jsx
+++ mondongo/src/pages/Homepage.jsx
@@ -1,11 +1,31 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Carousel from '../Components/Carousel/Carousel'
 import Categories from '../Components/Categories/Categories'; // Sección de categorías
 import Footer from '../Components/Footer/Footer'; // Pie de página
 import './Homepage.css'; // Estilos de la página de inicio
 
 
-function HomePage() {
+const HomePage() {
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
+    
+  const fetchProducts = async() => {
+    try {
+      const response = await fetch()
+      setProducts(response.data);
+      setLoading(false); 
+    } catch(error){
+      console.error('no se pudieron obtener los productos');
+      setLoading(false);
+    }
+  };
+  useEffect(() =>{
+    fetchProducts();
+  }, [])
+  
+
+
+
   return (
     <div>
       
@@ -16,6 +36,18 @@ function HomePage() {
          <Carousel/>
         </section>
         <Categories />
+        {loading ? (
+          <div className="loading">Cargando productos...Por Favor espere</div>
+        ) : (
+          <div className='product-list'>
+            {products.map((product) => (
+              <div key={product.id} className='product-item'>
+                <h2 className='product-name'>{product.name}</h2>
+                <p className='product-price'>${product.price}</p>
+              </div>
+            ))}
+          </div>
+        )}
       </main>
       <Footer />
     </div>
