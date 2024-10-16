d72a05d5d8bb36715426c37fdd1e0e19323a5762
Yaninna Alvarez <134446812+Yaninna137@users.noreply.github.com>
2024-10-16 16:13:43 -0300

    Solo sql carrito
    Se borro todo, y se dejja sql nomás.
    Más adelante se trabajara la lógica

--- carrito.sql
+++ carrito.sql
@@ -1,6 +1,8 @@
-CREATE TABLE Carrito (
-  ID_Carrito SERIAL PRIMARY KEY,
-  ID_Usuario INT NOT NULL,
-  Fecha_Creacion TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  FOREIGN KEY (ID_Usuario) REFERENCES Usuario(ID_Usuario)
+CREATE TABLE items_carrito (
+  id_carrito SERIAL PRIMARY KEY,                   
+  id_producto INT REFERENCES producto(id_producto),  
+  cantidad INT NOT NULL,                        
+  id_usuario UUID REFERENCES usuarios(id),      
+  fecha_creacion TIMESTAMP DEFAULT NOW()             
 );
+
--- carrito_producto.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE Carrito_Producto (
-  ID_Carrito INT NOT NULL,
-  ID_Producto INT NOT NULL,
-  Cantidad INT NOT NULL DEFAULT 1,
-  PRIMARY KEY (ID_Carrito, ID_Producto),
-  FOREIGN KEY (ID_Carrito) REFERENCES Carrito(ID_Carrito) ON DELETE CASCADE,
-  FOREIGN KEY (ID_Producto) REFERENCES Producto(ID_Producto)
-);
