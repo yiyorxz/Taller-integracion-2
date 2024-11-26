f5e592e1d7f6740ba59547951400a10525601ad1
javierrrp <javier.poblete2023@alu.uct.cl>
2024-11-25 21:18:20 -0300

    Explicacion de lo trabajado - Se mantiene la sesion iniciada hasta que uno cierre sesion, esta bien beta, asi q hay q mejorarlo - Lo que hice para asegurarme de que se mantuviese la sesion iniciada, fue que en el app, puse un texto que diga el nombre del usuario, y si no hay usuario iniciado de sesion, no muestra nada - Solo se puede agregar productos al carro si y solo si hay un usuario registrado
    Archivos:
    - mondongo\src\App.js
    - mondongo\src\Clientes\pages\loginpage.jsx
    - mondongo\src\Clientes\Components\Conex\UserContext.js
    - mondongo\src\Clientes\Components\Productsycarro\ProductList.jsx
    
    Cabe destacar, que claudio me habia pasado lo que estaba haciendo, entonces a partir de eso adapte al codigo, solo falta hacer la pagina del carrito

--- mondongo/src/Clientes/pages/Homepage.jsx
+++ mondongo/src/Clientes/pages/Homepage.jsx
@@ -6,7 +6,6 @@ import './Homepage.css'; // Estilos de la página de inicio
 import { Header } from '../Components/Productsycarro/Header';
 import { ProductList } from '../Components/Productsycarro/ProductList';
 import Popup from '../Components/PopUp/Popup'
-import { UserContext } from '../Components/Conex/UserContext';
 
 function HomePage() {
 
